169101ae35d70e2a2ac3e2d24023c49e8213931a
Owner <turnerkassie5@gmail.com>
2025-01-29 09:17:21 -0500

    added exporting route features

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(group = "com.graphhopper", name = "graphhopper-core", version = "7.0")
     implementation(group = "com.graphhopper", name = "graphhopper-reader-osm", version = "3.0-pre3")
     implementation(group = "org.slf4j", name = "slf4j-simple", version = "2.0.9")
+    implementation("org.json:json:20210307")
 }
 
 tasks.named<JavaExec>("run") {
---
+++ src/main/java/org/example/Route.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.List;
+
+public class Route {
+
+    List<Location> finalRoute;
+
+    Route(List<Location> route)
+    {
+        this.finalRoute = route;
+    }
+}
