f90d6aa4da33ecb3d340827922f02333f8cfd8d1
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-03-04 12:14:55 -0500

    Added JavaML Dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,13 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://bio.informatik.uni-jena.de/repository/libs-release-oss/")
+    }
 }
 
 dependencies {
+    implementation("net.sourceforge:javaml:0.1.7")
     implementation(group = "com.graphhopper", name = "graphhopper-core", version = "7.0")
     implementation(group = "com.graphhopper", name = "graphhopper-reader-osm", version = "3.0-pre3")
     implementation(group = "org.slf4j", name = "slf4j-simple", version = "2.0.9")
