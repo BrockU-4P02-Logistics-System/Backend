f91cd0dae86f20aef8a3e838f66c8366008e6f27
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-03-06 12:31:29 -0500

    Forgor to remove a dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,9 @@ plugins {
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://bio.informatik.uni-jena.de/repository/libs-release-oss/")
-    }
 }
 
 dependencies {
-    implementation("com.google.ortools:ortools-java:9.12.4544")
-    implementation("net.sourceforge:javaml:0.1.7")
     implementation(group = "com.graphhopper", name = "graphhopper-core", version = "7.0")
     implementation(group = "com.graphhopper", name = "graphhopper-reader-osm", version = "3.0-pre3")
     implementation(group = "org.slf4j", name = "slf4j-simple", version = "2.0.9")
