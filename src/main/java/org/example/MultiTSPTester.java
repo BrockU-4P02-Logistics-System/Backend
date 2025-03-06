4899ff1348aa6a1836a4ae73625f71a1e9157de2
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-03-06 11:56:04 -0500

    Created Multi-TSP and added a test case (Toronto Depot, 20 STC & 20 Barrie Stops, 2 Vehicles)

---
+++ src/main/java/org/example/MultiTSPTester.java
@@ -0,0 +1,86 @@
+package org.example;
+
+import com.graphhopper.GraphHopper;
+import com.graphhopper.config.CHProfile;
+import com.graphhopper.config.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiTSPTester {
+    private final GraphHopper hopper;
+    private final List<Location> locations;
+    private final int numVehicles;
+
+    public MultiTSPTester(int numVehicles, List<Location> locations) {
+        this.numVehicles = numVehicles;
+        this.locations = locations;
+
+        // Initialize GraphHopper
+        hopper = new GraphHopper();
+        hopper.setOSMFile("ontario.osm.pbf");
+        hopper.setGraphHopperLocation("graph-cache");
+        hopper.setProfiles(new Profile("car").setVehicle("car").setWeighting("fastest"));
+        hopper.getCHPreparationHandler().setCHProfiles(new CHProfile("car"));
+        hopper.importOrLoad();
+    }
+
+    public void solveRoutingProblem(int timeLimit) {
+        VehicleRouter router = new VehicleRouter(locations, hopper, numVehicles);
+        router.solveTSP(timeLimit);
+    }
+
+    public static void main(String[] args) {
+        List<Location> locations = new ArrayList<>();
+
+        //Toronto
+        locations.add(new Location(43.662500, -79.391111, 1)); // Queen's Park
+
+        // St. Catharines
+        locations.add(new Location(43.159374, -79.246862, 2)); // 1st Street Louth
+        locations.add(new Location(43.158912, -79.245678, 3)); // 2nd Avenue
+        locations.add(new Location(43.160123, -79.248456, 4)); // 3rd Avenue Louth
+        locations.add(new Location(43.161234, -79.249567, 5)); // 3rd Street
+        locations.add(new Location(43.162345, -79.250678, 6)); // 3rd Street Louth
+        locations.add(new Location(43.163456, -79.251789, 7)); // 4th Avenue
+        locations.add(new Location(43.164567, -79.252890, 8)); // 5th Avenue Louth
+        locations.add(new Location(43.165678, -79.253901, 9)); // 5th Street Louth
+        locations.add(new Location(43.166789, -79.254012, 10)); // 7 Oaks Circle
+        locations.add(new Location(43.167890, -79.255123, 11)); // 7th Street Louth
+        locations.add(new Location(43.168901, -79.256234, 12)); // 8th Avenue Louth
+        locations.add(new Location(43.169012, -79.257345, 13)); // Abbey Avenue
+        locations.add(new Location(43.170123, -79.258456, 14)); // Abbot Street West
+        locations.add(new Location(43.171234, -79.259567, 15)); // Abbott Street
+        locations.add(new Location(43.172345, -79.260678, 16)); // Aberdeen Circle
+        locations.add(new Location(43.173456, -79.261789, 17)); // Abraham Drive
+        locations.add(new Location(43.174567, -79.262890, 18)); // Academy Street
+        locations.add(new Location(43.175678, -79.263901, 19)); // Acadia Crescent
+        locations.add(new Location(43.176789, -79.264012, 20)); // Adams Street
+        locations.add(new Location(43.177890, -79.265123, 21)); // Addison Drive
+
+        // Barrie
+        locations.add(new Location(44.389123, -79.690456, 22)); // 10th Line
+        locations.add(new Location(44.388234, -79.689567, 23)); // Aconley Court
+        locations.add(new Location(44.387345, -79.688678, 24)); // Adam Avenue
+        locations.add(new Location(44.386456, -79.687789, 25)); // Adam Drive
+        locations.add(new Location(44.385567, -79.686890, 26)); // Adam Street
+        locations.add(new Location(44.384678, -79.685901, 27)); // Addison Trail
+        locations.add(new Location(44.383789, -79.684012, 28)); // Adelaide Street
+        locations.add(new Location(44.382890, -79.683123, 29)); // Agnes Street
+        locations.add(new Location(44.381901, -79.682234, 30)); // Aikens Crescent
+        locations.add(new Location(44.380912, -79.681345, 31)); // Albert Street
+        locations.add(new Location(44.379923, -79.680456, 32)); // Aldergrove Close
+        locations.add(new Location(44.378934, -79.679567, 33)); // Aleda Street
+        locations.add(new Location(44.377945, -79.678678, 34)); // Alexander Avenue
+        locations.add(new Location(44.376956, -79.677789, 35)); // Alfred Street
+        locations.add(new Location(44.375967, -79.676890, 36)); // Algonquin Trail
+        locations.add(new Location(44.374978, -79.675901, 37)); // Alliance Boulevard
+        locations.add(new Location(44.373989, -79.674012, 38)); // Allsop Crescent
+        locations.add(new Location(44.372990, -79.673123, 39)); // Alva Street
+        locations.add(new Location(44.371901, -79.672234, 40)); // Ambler Bay
+        locations.add(new Location(44.370912, -79.671345, 41)); // Amelia Street
+
+        MultiTSPTester tester = new MultiTSPTester(2, locations);
+        tester.solveRoutingProblem(20);
+    }
+}
---
+++ src/main/java/org/example/VehicleRouter.java
@@ -0,0 +1,91 @@
+package org.example;
+
+import com.google.protobuf.Duration;
+import com.graphhopper.GraphHopper;
+import com.google.ortools.Loader;
+import com.google.ortools.constraintsolver.*;
+
+import java.util.List;
+
+public class VehicleRouter {
+    private final List<Location> locations;
+    private final int[][] distanceMatrix;
+    private final int num_vehicles;
+
+    public VehicleRouter(List<Location> locations, GraphHopper hopper, int num_vehicles) {
+        this.locations = locations;
+        this.num_vehicles = num_vehicles;
+
+        // Use the PrecomputedDistance class
+        PrecomputedDistance distances = new PrecomputedDistance(locations, hopper);
+        this.distanceMatrix = distances.distanceMatrix;
+    }
+
+    public void solveTSP(int timeLimit) {
+        Loader.loadNativeLibraries(); // Load OR-Tools
+
+        int numLocations = distanceMatrix.length;
+        System.out.println("Solving for " + numLocations + " locations with " + num_vehicles + " vehicles");
+
+        // Create routing index manager
+        // For a multiple vehicle routing problem with a depot
+        RoutingIndexManager manager = new RoutingIndexManager(numLocations, num_vehicles, 0);
+
+        // Create routing model
+        RoutingModel routing = new RoutingModel(manager);
+
+        // Create and register a transit callback
+        final int transitCallbackIndex = routing.registerTransitCallback((long fromIndex, long toIndex) -> {
+            // Convert from routing variable Index to user NodeIndex
+            int fromNode = manager.indexToNode((int) fromIndex);
+            int toNode = manager.indexToNode((int) toIndex);
+            return distanceMatrix[fromNode][toNode];
+        });
+
+        // Define cost of each arc
+        routing.setArcCostEvaluatorOfAllVehicles(transitCallbackIndex);
+
+        // Add Distance constraint
+        routing.addDimension(transitCallbackIndex, 0, 10000000, true, "Distance");
+        RoutingDimension distanceDimension = routing.getMutableDimension("Distance");
+        distanceDimension.setGlobalSpanCostCoefficient(100);
+
+        // Setting first solution heuristic
+        RoutingSearchParameters searchParameters =
+                main.defaultRoutingSearchParameters()
+                        .toBuilder()
+                        .setFirstSolutionStrategy(FirstSolutionStrategy.Value.PATH_CHEAPEST_ARC)
+                        .setTimeLimit(Duration.newBuilder().setSeconds(timeLimit).build())
+                        .build();
+
+        // Solve the problem
+        Assignment solution = routing.solveWithParameters(searchParameters);
+
+        // Print solut  ion
+        if (solution != null) {
+            // Print routes
+            for (int i = 0; i < num_vehicles; i++) {
+                System.out.print("Route for Vehicle " + (i) + ": ");
+                long index = routing.start(i);
+                String route = "";
+                long routeDistance = 0;
+
+                while (!routing.isEnd(index)) {
+                    int routeIndex = manager.indexToNode((int) index);
+                    route += routeIndex+1+ " -> ";//manager.indexToNode((int) index) + " -> ";
+                    long previousIndex = index;
+                    index = solution.value(routing.nextVar(index));
+                    routeDistance += routing.getArcCostForVehicle(previousIndex, index, i);
+                }
+                int finalIndex = manager.indexToNode((int) index);
+                route += finalIndex+1;
+                System.out.println(route);
+                System.out.println("Distance of route: " + routeDistance);
+            }
+
+            System.out.println("Total Distance of all routes: " + solution.objectiveValue());
+        } else {
+            System.out.println("No solution found!");
+        }
+    }
+}
