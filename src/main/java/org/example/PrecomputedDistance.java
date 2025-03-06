df8e2a924f01e7f6f471ea85caf75ed815fed460
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-03-06 12:00:22 -0500

    cache was unnecessary for mTSP so i removed it

--- src/main/java/org/example/PrecomputedDistance.java
+++ src/main/java/org/example/PrecomputedDistance.java
@@ -10,12 +10,10 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.List;
 
 public class PrecomputedDistance {
-    public ConcurrentHashMap<String, Long> cache;
     public int[][] distanceMatrix;
     // Constructor: Stores a reference to the precomputed n×n distance matrix
     public PrecomputedDistance(List<Location> locations, GraphHopper hopper) {
         int n = locations.size();
-        this.cache = new ConcurrentHashMap<>((n*n)-n);
         this.distanceMatrix = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -29,18 +27,11 @@ public class PrecomputedDistance {
     }
 
     public int computeDistance(Location a, Location b, GraphHopper hopper) {
-        StringBuilder keyBuilder = new StringBuilder();
-        keyBuilder.append(a.getID());
-        keyBuilder.append("-");
-        keyBuilder.append(b.getID());
-        String key = keyBuilder.toString();
-        if(cache.containsKey(key)) return cache.get(key).intValue();
         if (a.getID() == b.getID()) return 0;
         GHRequest req = new GHRequest(a.getLat(), a.getLon(), b.getLat(), b.getLon());
         req.setProfile("car");
         GHResponse res = hopper.route(req);
         ResponsePath path = res.getBest();
-        cache.put(key, path.getTime() / 60000);
         return (int) path.getTime();
     }
 
