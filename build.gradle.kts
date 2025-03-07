ed3c117f3a08f5d89e94dc5b99b0e41e8d685757
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-03-07 10:04:55 -0500

    Fixed dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.ortools:ortools-java:9.12.4544")
     implementation(group = "com.graphhopper", name = "graphhopper-core", version = "7.0")
     implementation(group = "com.graphhopper", name = "graphhopper-reader-osm", version = "3.0-pre3")
     implementation(group = "org.slf4j", name = "slf4j-simple", version = "2.0.9")
